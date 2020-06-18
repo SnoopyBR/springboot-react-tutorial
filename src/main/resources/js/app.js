db5ba33414fec94d742e35154754016c1a4257ba
Julio Pedroso <juliopedroso@gmail.com>
2020-06-17 23:48:22 -0300

    last review

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -1,70 +1,11 @@
 import React from 'react';
-import { render as _render } from 'react-dom';
-import client from './client';
 
-class App extends React.Component{
+import Routes from './routes';
 
-
-     constructor(props){
-         super(props); 
-         this.state = {employees:[]};
-     }
-
-    componentDidMount(){
-        console.log('running componentDidMount'); 
-        client({method: 'GET', path: '/api/employees'}).then(response =>{
-            console.log(response);
-            this.setState({
-                employees : response.entity._embedded.employees
-                
-            });
-            console.log(employees)
-        });
-    }
-
-    render(){
-        return (
-            <EmployeeList employees = {this.state.employees}/>
-        )
-    }
-
+function App(){
+    return (
+        <Routes/>
+    );
 }
 
-class EmployeeList extends React.Component{
-    render (){
-        const employees = this.props.employees.map(employee => {
-            return   <Employee key={employee._links.self.href} employee = {employee}/>
-        });
-
-        return (
-            <table>
-                <tbody>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Description</th>
-                    </tr>
-
-                    {employees}
-                </tbody>
-            </table>                
-        )
-    }
-}
-
-class Employee extends React.Component {
-    render(){
-        return(
-            <tr>
-                <td>{this.props.employee.firstName}</td>
-                <td>{this.props.employee.lastName}</td>
-                <td>{this.props.employee.description}</td>
-            </tr>
-        )
-    }
-}
-
-_render(
-    <App />,
-    document.getElementById('react')
-)
+export default App;
--- src/main/resources/js/pages/employees/index.js
+++ src/main/resources/js/pages/employees/index.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {FiDelete} from 'react-icons/fi';
 
-import api from '../services/api';
+import api from '../../services/api';
 
-export default function employee(){
-    return (
+export default function Employee() {
+    
+    const [employees,setEmployees] = useState([]);
+    useEffect(()=>{
+
+        api.get('employees')
+        .then(response =>{
+            //response._embedded.employees
+            console.log(response);
+            setEmployees(response.data._embedded.employees);
+        })
+
         
+    },[]);
+
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Description</th>
+                    <th>Actions</th>
+                </tr>
+                {console.log(employees),
+                employees.map(employee =>(
+                    
+                    <tr key= {employee._links.self.href}>
+                        <td>{employee.firstName}</td>
+                        <td>{employee.lastName}</td>
+                        <td>{employee.description}</td>
+                        <td><a name="" id="employee._links.self.href" className="btn btn-danger" href="#" role="button"><FiDelete /></a></td>
+                    </tr>
+                ))}
+
+            </tbody>
+        </table>
     )
 }
